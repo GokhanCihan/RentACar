e227b2e6cb30a66d4c0e2f1890294df8ab062000
Gökhan Cihan <83141061+GokhanCihan@users.noreply.github.com>
2024-04-02 12:37:52 +0300

    create dao for users

---
+++ src/dao/UserDao.java
@@ -0,0 +1,59 @@
+package dao;
+
+import core.DBConnection;
+import entities.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class UserDao {
+    private final Connection connection;
+
+    public UserDao() {
+        this.connection = DBConnection.getInstance();
+    }
+
+    private ArrayList<User> findAll() {
+        ArrayList<User> users = new ArrayList<>();
+        String query = "SELECT * FROM public.users ";
+        try {
+            ResultSet resultSet = this.connection.createStatement().executeQuery(query);
+            while (resultSet.next()) {
+                User user = this.match(resultSet);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
+
+    public User login(String username, String password) {
+        User user = null;
+
+        String query = "SELECT * FROM public.users WHERE username = ? AND password = ?";
+        try {
+            PreparedStatement preparedStatement = this.connection.prepareStatement(query);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                user = this.match(resultSet);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+    public User match(ResultSet resultSet) throws SQLException {
+        User user = new User();
+        user.setId(resultSet.getInt("user_id"));
+        user.setUsername(resultSet.getString("username"));
+        user.setPassword(resultSet.getString("password"));
+        user.setRole(resultSet.getString("role"));
+        return user;
+    }
+}
