9a0086d03bdb3325d41acf03892486e185a4f5b8
Gökhan Cihan <83141061+GokhanCihan@users.noreply.github.com>
2024-04-02 21:46:01 +0300

    make id's serial

--- rentacar.sql
+++ rentacar.sql
@@ -1,18 +1,18 @@
 -- following tables created after the creation of the database using PostgreSQL 16
 
 CREATE TABLE brands (
-    brand_id int NOT NULL,
+    brand_id SERIAL,
     name text NOT NULL,
     PRIMARY KEY (brand_id)
 );
 
 CREATE TABLE models (
-    model_id int NOT NULL,
+    model_id SERIAL,
     brand_id int NOT NULL,
     name text NOT NULL,
     release_year int NOT NULL,
     body_type text NOT NULL,
-    fuel_type text NOT NULL, 
+    fuel_type text NOT NULL,
     gear_type text NOT NULL,
     PRIMARY KEY (model_id),
     FOREIGN KEY (brand_id) REFERENCES brands(brand_id),
@@ -20,19 +20,19 @@ CREATE TABLE models (
 );
 
 CREATE TABLE cars (
-    car_id int NOT NULL,
+    car_id SERIAL,
     brand_id int NOT NULL,
     model_id int NOT NULL,
     color text NOT NULL,
     mileage_km int NOT NULL,
-    Plate text NOT NULL UNIQUE, 
+    Plate text NOT NULL UNIQUE,
     PRIMARY KEY (car_id),
     FOREIGN KEY (brand_id) REFERENCES brands(brand_id),
     FOREIGN KEY (model_id) REFERENCES models(model_id)
 );
 
 CREATE TABLE bookings (
-    booking_id int NOT NULL,
+    booking_id SERIAL,
     car_id int NOT NULL,
     name text NOT NULL,
     phone int NOT NULL,
@@ -47,12 +47,12 @@ CREATE TABLE bookings (
 );
 
 CREATE TABLE users (
-    user_id int NOT NULL,
+    user_id SERIAL,
     username text NOT NULL,
     password text NOT NULL,
     role text NOT NULL,
     PRIMARY KEY (user_id)
 );
 
-INSERT INTO users(user_id, username, password, role) VALUES (1, 'admin', 'admin', 'admin');
-INSERT INTO brands(brand_id, name) VALUES (1, 'Renault');
+INSERT INTO users(user_id, username, password, role) VALUES ('admin', 'admin', 'admin');
+INSERT INTO brands(name) VALUES ('Renault');
